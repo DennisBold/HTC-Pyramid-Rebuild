a756c7e93fd154d70bbbde548b7732fadde044bb
Dennis Bold <dennis@dennisbold.com>
2013-04-10 22:53:04 +0200

    Correct TARGET_KERNEL_SOURCE
    Kernel Source (MSM for all boards) is located in AndroidSource/kernel/

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ TARGET_BOOTLOADER_BOARD_NAME := pyramid
 BOARD_KERNEL_BASE := 0x48000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0 android.boot.hardware=pyramid no_console_suspend=1
-TARGET_KERNEL_SOURCE := kernel/msm
+TARGET_KERNEL_SOURCE := kernel
 TARGET_KERNEL_CONFIG := pyramid_defconfig
 
 # BOOT
